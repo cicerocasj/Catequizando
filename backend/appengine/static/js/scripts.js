12ff1a71fc5a5106a3fd86389f4cbf627a4ebec9
cicerocasj <cicerocasj@gmail.com>
2015-03-07 18:08:37 -0300

    fazendo o fake input checkbox

--- backend/appengine/static/js/scripts.js
+++ backend/appengine/static/js/scripts.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
     // clique do checkbox --- checked active
     var input_fake_checkbox = $("ins.iCheck-helper");
     input_fake_checkbox.click(function(e){
-        console.log("cicero");
-
-        //$(e).hasClass("checked");
+        var input_fake = $(this).siblings("input");
+        if(input_fake.attr("checked") === "checked"){
+            console.log("sim");
+            input_fake.attr("checked", " ");
+        } else {
+            console.log("nao");
+            input_fake.attr("checked", "checked");
+        }
     });
 
     var input_checkbox = $("input.icheck");
