889a199b88442a39b95d3bccab01ad5246692459
cicerocasj <cicerocasj@gmail.com>
2015-03-08 21:56:28 -0300

    pronto checkebox fake

--- backend/appengine/static/js/scripts.js
+++ backend/appengine/static/js/scripts.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
-    // clique do checkbox --- checked active
+    // clique do checkbox
     var input_fake_checkbox = $("ins.iCheck-helper");
-    input_fake_checkbox.click(function(e){
+    input_fake_checkbox.click(function(){
         var input_fake = $(this).siblings("input");
-        if(input_fake.attr("checked") === "checked"){
-            console.log("sim");
-            input_fake.attr("checked", " ");
-        } else {
-            console.log("nao");
+        if(typeof input_fake.attr("checked") === typeof undefined){
             input_fake.attr("checked", "checked");
+        } else {
+            input_fake.removeAttr("checked");
         }
-    });
-
-    var input_checkbox = $("input.icheck");
-    input_checkbox.click(function(e){
-        console.log("cicero");
-        $(e).hasClass("checked");
     });
 });
