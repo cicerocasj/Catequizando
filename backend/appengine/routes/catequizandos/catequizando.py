106c23e5da3763d70f1f6334625b94ae97485df2
cicerocasj <cicerocasj@gmail.com>
2015-03-21 10:10:03 -0300

    cadastrando catequizandos, mas sem foto

--- backend/appengine/routes/catequizandos/catequizando.py
+++ backend/appengine/routes/catequizandos/catequizando.py
@@ -1,11 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from catequizando_app import catequizando_commands, catequizando_facade
+from catequizando_app.catequizando_model import Catequizando
+from gaebusiness.business import CommandExecutionException
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required, permissions
 from config.template_middleware import TemplateResponse
+from routes import catequizandos
+from tekton import router
+from tekton.gae.middleware.redirect import RedirectResponse
 
 
-@login_not_required
 @no_csrf
 def index(id=0):
     context = {}
@@ -14,21 +19,19 @@ def index(id=0):
     except Exception as e:
         key_id = None
     if key_id:
-        context["id"] = id
-        context["name"] = u'Osvaldo Brandão'
-        context["email"] = u'osvaldinho@gmail.com'
-        context["address"] = u'Avenida Juscelino Kubitscheck, 9999, Vila Industrial - São José dos Campos'
-        context["phone"] = u'3333-3333'
-        context["cellphone"] = u'98888-8888'
-        context["avatar"] = u'avatar3.jpg'
-        context["group"] = {
-            'type': 'crisma 3',
-            'day': 'quarta',
-            'hours': '19:00 - 20:00',
-            'local': u'paróquia',
-            'catechized': 16,
-        }
+        context["catechized"] = Catequizando.get_by_id(key_id)
+
     context["nav_active"] = 'catequizandos'
-    context["responsible_1"] = {'name': u'José Carlos', 'phone': '3333-3333'}
-    context["responsible_2"] = {'name': u'Maria das Dores', 'phone': '3333-3333'}
+    context["save_path"] = router.to_path(save)
     return TemplateResponse(context, template_path='/catequizandos/catequizando.html')
+
+
+def save(**catequizando_properties):
+    cmd = catequizando_facade.save_catequizando_cmd(**catequizando_properties)
+    try:
+        cmd()
+    except CommandExecutionException:
+        context = {'errors': cmd.errors,
+                   'catechized': catequizando_properties}
+        return TemplateResponse(context, '/catequizandos/catequizando.html')
+    return RedirectResponse(router.to_path(catequizandos))
--- backend/appengine/routes/catequizandos/home.py
+++ backend/appengine/routes/catequizandos/home.py
@@ -1,13 +1,16 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+from catequizando_app.catequizando_model import Catequizando
 from gaecookie.decorator import no_csrf
 from gaepermission.decorator import login_not_required, permissions
 from config.template_middleware import TemplateResponse
 
 
-@login_not_required
 @no_csrf
 def index():
     context = {}
+    query = Catequizando.query_by_creation_desc()
+    catechizeds = query.fetch()
+    context["catechizeds"] = catechizeds
     context["nav_active"] = 'catequizandos'
     return TemplateResponse(context, template_path='/catequizandos/index.html')
