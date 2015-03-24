8592d8a2f8afec9e5a6a7f5f559e6319734d3809
cicerocasj <cicerocasj@gmail.com>
2015-03-24 12:00:41 -0300

    selecionando foto ao clicar na imagem

--- backend/appengine/static/js/scripts.js
+++ backend/appengine/static/js/scripts.js
@@ -4,4 +4,16 @@ $(document).ready(function(){
     input_fake_checkbox.click(function(){
         $(this).siblings("input").attr('checked', $(this).parent().hasClass('checked'));
     });
+    var input_avatar = $("#input_avatar");
+    var fake_avatar = $("#fake_avatar");
+    fake_avatar.click(function(){
+        input_avatar.click();
+    });
+    input_avatar.change(function(){
+        var img = new FileReader();
+        img.onload(function(e){
+            fake_avatar.attr("src", e.target.result);
+        });
+        img.readAsDataURL(input_avatar.file())
+    });
 });
