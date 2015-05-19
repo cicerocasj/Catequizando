9486a08594775cf8eac7e8560b88888ed69b967c
Cícero Alves <cicerocasj@gmail.com>
2015-05-19 12:20:22 -0300

    voltando para commit anterior e criando profile
    evento melhorado

---
+++ backend/appengine/routes/profiles/edit.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from config.template_middleware import TemplateResponse
+from gaebusiness.business import CommandExecutionException
+from tekton import router
+from gaecookie.decorator import no_csrf
+from profile_app import profile_facade
+from routes import profiles
+from tekton.gae.middleware.redirect import RedirectResponse
+
+
+@no_csrf
+def index(profile_id):
+    profile = profile_facade.get_profile_cmd(profile_id)()
+    profile_form = profile_facade.profile_form()
+    context = {'save_path': router.to_path(save, profile_id), 'profile': profile_form.fill_with_model(profile)}
+    return TemplateResponse(context, 'profiles/profile_form.html')
+
+
+def save(profile_id, **profile_properties):
+    cmd = profile_facade.update_profile_cmd(profile_id, **profile_properties)
+    try:
+        cmd()
+    except CommandExecutionException:
+        context = {'errors': cmd.errors, 'profile': profile_properties}
+
+        return TemplateResponse(context, 'profiles/profile_form.html')
+    return RedirectResponse(router.to_path(profiles))
+
---
+++ backend/appengine/routes/profiles/rest.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from gaebusiness.business import CommandExecutionException
+from tekton.gae.middleware.json_middleware import JsonResponse
+from profile_app import profile_facade
+
+
+def index():
+    cmd = profile_facade.list_profiles_cmd()
+    profile_list = cmd()
+    profile_form = profile_facade.profile_form()
+    profile_dcts = [profile_form.fill_with_model(m) for m in profile_list]
+    return JsonResponse(profile_dcts)
+
+
+def new(_resp, **profile_properties):
+    cmd = profile_facade.save_profile_cmd(**profile_properties)
+    return _save_or_update_json_response(cmd, _resp)
+
+
+def edit(_resp, id, **profile_properties):
+    cmd = profile_facade.update_profile_cmd(id, **profile_properties)
+    return _save_or_update_json_response(cmd, _resp)
+
+
+def delete(_resp, id):
+    cmd = profile_facade.delete_profile_cmd(id)
+    try:
+        cmd()
+    except CommandExecutionException:
+        _resp.status_code = 500
+        return JsonResponse(cmd.errors)
+
+
+def _save_or_update_json_response(cmd, _resp):
+    try:
+        profile = cmd()
+    except CommandExecutionException:
+        _resp.status_code = 500
+        return JsonResponse(cmd.errors)
+    profile_form = profile_facade.profile_form()
+    return JsonResponse(profile_form.fill_with_model(profile))
+
